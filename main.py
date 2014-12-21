24e6ba18e79086232821d864efbde0e5d1ce7b0b
youngsterxyf <sas.198708@gmail.com>
2014-12-21 22:14:59 +0800

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+__author__ = 'xiayf'
+
+import psutil
+
+
+class BeautyEye(object):
+
+    def __init__(self):
+        pass
+
+    def __cpu_stat(self):
+        pass
+
+    def __mem_stat(self):
+        pass
+
+    def __disk_stat(self):
+        pass
+
+    def blink(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
