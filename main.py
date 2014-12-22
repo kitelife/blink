bc2ef102142195bab4c41ff8542577183a1efb11
youngsterxyf <sas.198708@gmail.com>
2014-12-23 01:58:19 +0800

    update

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ __author__ = 'xiayf'
 
 import multiprocessing
 import time
+from datetime import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 import psutil
 
@@ -11,7 +15,6 @@ import psutil
 class BeautyEye(object):
 
     def __init__(self, config):
-        print config
         self.config = config
 
     def __cpu_stat(self):
@@ -35,6 +38,35 @@ class BeautyEye(object):
                 print dp.mountpoint, psutil.disk_usage(dp.mountpoint).percent, '%'
             time.sleep(stat_interval)
 
+    def __plot_data(self):
+        pass
+
+    def __render_content(self):
+        content = ''
+        return content
+
+    def __prepare_data(self):
+        return ''
+
+    def __email_it(self):
+        email_content = self.__prepare_data()
+
+        msg = MIMEText(email_content, 'html', 'utf-8')
+        msg['Subject'] = Header(self.config['email']['subject'] % (datetime.strftime('%Y-%m-%d %H:%M:%S'), ), 'utf-8')
+        msg['From'] = Header(self.config['email']['from'], 'utf-8')
+        msg['To'] = ', '.join(self.config['email']['to'])
+
+        try:
+            email_server_info = self.config['email']['server']
+            s = smtplib.SMTP(email_server_info['host'], email_server_info['port'])
+            s.login(email_server_info['username'], email_server_info['password'])
+            s.sendmail(self.config['email']['from'], self.config['email']['to'], msg.as_string())
+            s.quit()
+        except Exception as e:
+            print e
+            return False
+        return True
+
     def blink(self):
         process_record = []
         for m in [self.__cpu_stat, self.__mem_stat, self.__disk_stat]:
@@ -55,6 +87,17 @@ def main():
         },
         'disk': {
             'interval': 5,
+        },
+        'email': {
+            'server': {
+                'host': '',
+                'port': '',
+                'username': '',
+                'password': '',
+            },
+            'subject': '服务器监控数据汇总 - %s',
+            'from': '',
+            'to': [],
         }
     }
     be = BeautyEye(config)
