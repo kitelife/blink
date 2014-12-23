2bb3e2ca5dd5838c4f00b1ee71040ee51352f279
xiayongfeng <xiayongfeng@baidu.com>
2014-12-23 19:43:59 +0800

    基本完成，待调试

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ from datetime import datetime
 from email.mime.text import MIMEText
 from email.header import Header
 from collections import OrderedDict
+from string import Template
 
 import psutil
+import vincent
 
 
 class DB(object):
@@ -23,7 +25,7 @@ class DB(object):
         self.__prepare()
 
     def __prepare(self):
-        table_cpu = '''CREATE TABLE cpu_stat IF NOT EXIST (
+        table_cpu = '''CREATE TABLE IF NOT EXIST cpu_stat (
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             used_percent INTEGER NOT NULL DEFAULT -1,
             created_at TIMESTAMP
@@ -32,7 +34,7 @@ class DB(object):
         self.__cursor.execute(table_cpu)
         self.__conn.commit()
 
-        table_mem = '''CREATE TABLE mem_stat IF NOT EXIST (
+        table_mem = '''CREATE TABLE IF NOT EXIST mem_stat (
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             used_percent INTEGER NOT NULL DEFAULT -1,
             created_at TIMESTAMP
@@ -106,7 +108,10 @@ class BeautyEye(object):
 
             cpu_stat_queue.add(cpu_usage_percent)
             if functools.reduce(self.__over_threshold, cpu_stat_queue.data, 0) == abnormal_continue_times:
-                pass
+                email_subject = u'服务器CPU使用率告警 - %s' % (datetime.strftime('%Y-%m-%d %H:%M:%S'), )
+                email_content = u'CPU使用率已连续 {abnormal_times} 次超过 {usage_threshold}%'\
+                    .format(abnormal_times=abnormal_continue_times, usage_threshold=90)
+                self.__email_alert(subject=email_subject, content=email_content)
 
             time.sleep(stat_interval)
 
@@ -125,7 +130,10 @@ class BeautyEye(object):
 
             mem_stat_queue.add(mem_usage_percent)
             if functools.reduce(self.__over_threshold, mem_stat_queue.data, 0) == abnormal_continue_times:
-                pass
+                email_subject = u'服务器内存使用率告警 - %s' % (datetime.strftime('%Y-%m-%d %H:%M:%S'), )
+                email_content = u'内存使用率已连续 {abnormal_times} 次超过 {usage_threshold}%'\
+                    .format(abnormal_times=abnormal_continue_times, usage_threshold=90)
+                self.__email_alert(subject=email_subject, content=email_content)
 
             time.sleep(stat_interval)
 
@@ -138,25 +146,25 @@ class BeautyEye(object):
                 if usage_percent > disk_threshold:
                     abnormal_mountpoint[mount_point] = usage_percent
             if len(abnormal_mountpoint) > 0:
-                pass
+                email_subject = u'服务器磁盘告警 - %s' % (datetime.strftime('%Y-%m-%d %H:%M:%S'), )
+                email_content_parts = []
+                for mount_point, usage_percent in abnormal_mountpoint.iteritems():
+                    email_content_parts.append('{mount_point}: {usage_percent}%'
+                                               .format(mount_point=mount_point, usage_percent=usage_percent))
+                email_content = '<br />'.join(email_content_parts)
+                self.__email_alert(email_subject, email_content)
 
             time.sleep(stat_interval)
 
-    def __plot_data(self):
-        pass
+    @staticmethod
+    def __render_content(template, data):
+        with open(template) as fh:
+            template = Template(str(fh.readall()))
+            return template.substitute(cpu_stat_data=data['cpu_stat'], mem_stat_data=data['mem_stat'])
 
-    def __render_content(self, template, data):
-        content = ''
-        return content
-
-    def __prepare_data(self):
-        return ''
-
-    def __email_it(self):
-        email_content = self.__prepare_data()
-
-        msg = MIMEText(email_content, 'html', 'utf-8')
-        msg['Subject'] = Header(self.__config['email']['subject'] % (datetime.strftime('%Y-%m-%d %H:%M:%S'), ), 'utf-8')
+    def __email_it(self, subject, content):
+        msg = MIMEText(content, 'html', 'utf-8')
+        msg['Subject'] = Header(subject, 'utf-8')
         msg['From'] = Header(self.__config['email']['from'], 'utf-8')
         msg['To'] = ', '.join(self.__config['email']['to'])
 
@@ -171,9 +179,57 @@ class BeautyEye(object):
             return False
         return True
 
+    def __email_alert(self, subject, content):
+        self.__email_it(subject=subject, content=content)
+
+    def __email_summary(self):
+        now = datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+        stat_data = {
+            'cpu_stat': {
+                'used_percent': [],
+                'created_at': []
+            },
+            'mem_stat': {
+                'used_percent': [],
+                'created_at': []
+            }
+        }
+        for table_name in stat_data.keys():
+            for_select = 'SELECT used_percent, created_at FROM %s WHERE created_at < "%s" ORDER BY created_at' \
+                         % (table_name, now)
+            for row in self.__db.query(for_select):
+                stat_data[table_name]['used_percent'].append(row[0])
+                stat_data[table_name]['created_at'].append(row[1])
+            for_delete = 'DELETE FROM %s WHERE created_at < "%s' % (table_name, now)
+            self.__db.execute(for_delete)
+
+        # cpu
+        cpu_stat_data = stat_data['cpu_stat']
+        cpu_graph = vincent.Area(cpu_stat_data['used_percent'])
+        cpu_graph.axis_titles(x=u'时间', y=u'使用率(%)')
+        cpu_graph.name(u'CPU使用率')
+        cpu_graph_json = cpu_graph.to_json()
+
+        # memory
+        mem_stat_data = stat_data['mem_stat']
+        mem_graph = vincent.Area(mem_stat_data['used_percent'])
+        mem_graph.axis_titles(x=u'时间', y=u'使用率(%)')
+        mem_graph.name(u'内存使用率')
+        mem_graph_json = mem_graph.to_json()
+
+        email_content = self.__render_content(template='templates/monitor_stat.html',
+                              data={'cpu_stat': cpu_graph_json, 'mem_graph_json': mem_graph_json})
+        if email_content is None:
+            print u'模板渲染失败！'
+            return False
+        email_subject = '服务器监控数据'
+        self.__email_it(subject=email_subject, content=email_content)
+        return True
+
     def blink(self):
         process_record = []
-        for m in [self.__cpu_stat, self.__mem_stat, self.__disk_stat]:
+        for m in [self.__cpu_stat, self.__mem_stat, self.__disk_stat, self.__email_summary]:
             p = multiprocessing.Process(target=m)
             p.start()
             process_record.append(p)
@@ -190,23 +246,22 @@ def main():
             'interval': 1,
         },
         'disk': {
-            'interval': 5,
+            'interval': 3600 * 4,
         },
         'email': {
+            'interval': 3600 * 24,
             'server': {
                 'host': '',
                 'port': '',
                 'username': '',
                 'password': '',
             },
-            'subject': '服务器监控数据汇总 - %s',
             'from': '',
             'to': [],
         }
     }
     be = BeautyEye(config)
     be.blink()
-
 
 if __name__ == '__main__':
     main()
